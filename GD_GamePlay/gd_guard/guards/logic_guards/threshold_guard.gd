ca7a6eaa3c2e54acd06b471d27e9cb034cca0e2b
youer0219 <xlm20040219@qq.com>
2025-08-23 11:24:08 +0800

    GD_Guard：添加门限守卫

---
+++ GD_GamePlay/gd_guard/guards/logic_guards/threshold_guard.gd
@@ -0,0 +1,18 @@
+extends GD_LogicGuard
+class_name GD_ThresholdGuard
+## 门限守卫
+
+@export var guards: Array[GD_Guard] = []
+@export_range(0,INF,1.0) var need_guard_num:int = 1
+
+func is_satisfied(guard_group:GD_GuardGroup,context:Dictionary = {}) -> bool:
+	if guards.size() < need_guard_num:
+		push_error("No enough guards provided to ThresholdGuard")
+		return false
+	var curr_num := 0
+	for guard in guards:
+		if guard.is_satisfied(guard_group,context):
+			curr_num += 1
+			if curr_num >= need_guard_num:
+				return true
+	return false
